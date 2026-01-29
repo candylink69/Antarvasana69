43506208ce50981868d5dab65aec3f61361a3a5b
216120306076 <omomshanti079@gmail.com>
2026-01-29 18:07:21 +0530

    Create AdSmartLink.tsx

---
+++ src/components/ads/AdSmartLink.tsx
@@ -0,0 +1,8 @@
+// src/components/ads/AdSmartLink.tsx
+import AdSlot from './AdSlot';
+
+const AdSmartLink = () => {
+  return <AdSlot position="smartLink" className="my-8" />;
+};
+
+export default AdSmartLink;
