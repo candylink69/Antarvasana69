dcddf907eebb19877f234c6f7401939b242d0f3c
216120306076 <omomshanti079@gmail.com>
2026-01-29 18:04:38 +0530

    Create AdStickyBottom.tsx

---
+++ src/components/ads/AdStickyBottom.tsx
@@ -0,0 +1,8 @@
+// src/components/ads/AdListBanner.tsx
+import AdSlot from './AdSlot';
+
+const AdListBanner = () => {
+  return <AdSlot position="listBanner" className="my-6" />;
+};
+
+export default AdListBanner;
