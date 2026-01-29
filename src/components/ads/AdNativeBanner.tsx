9c96e9464a2d210ebb093199199ac5d1d9208e7a
216120306076 <omomshanti079@gmail.com>
2026-01-29 18:08:15 +0530

    Create AdNativeBanner.tsx

---
+++ src/components/ads/AdNativeBanner.tsx
@@ -0,0 +1,8 @@
+// src/components/ads/AdNativeBanner.tsx
+import AdSlot from './AdSlot';
+
+const AdNativeBanner = () => {
+  return <AdSlot position="nativeBanner" className="my-6" />;
+};
+
+export default AdNativeBanner;
