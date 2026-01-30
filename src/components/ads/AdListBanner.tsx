c7b615856a04d93fff8cf1a206e92a28f81314f3
216120306076 <omomshanti079@gmail.com>
2026-01-30 15:23:24 +0530

    Delete src/components/ads/AdListBanner.tsx

--- src/components/ads/AdListBanner.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/ads/AdListBanner.tsx
-import AdSlot from './AdSlot';
-
-const AdListBanner = () => {
-  return <AdSlot position="listBanner" className="my-6" />;
-};
-
-export default AdListBanner;
