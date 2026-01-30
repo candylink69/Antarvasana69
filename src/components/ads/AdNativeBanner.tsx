70ba71152c8c19be6a3c5498eac2e38d1c75f638
216120306076 <omomshanti079@gmail.com>
2026-01-30 15:23:38 +0530

    Delete src/components/ads/AdNativeBanner.tsx

--- src/components/ads/AdNativeBanner.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/ads/AdNativeBanner.tsx
-import AdSlot from './AdSlot';
-
-const AdNativeBanner = () => {
-  return <AdSlot position="nativeBanner" className="my-6" />;
-};
-
-export default AdNativeBanner;
